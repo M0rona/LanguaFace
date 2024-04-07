b9246c037ccc552814f1ec3122d2fc646930c1ca
Lorrust <lucasrs202@gmail.com>
2024-04-07 14:18:35 -0300

    Criação da classse Translator e exemplo de utilização

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# UI
-#import tkinter
+from translate import Translator
+
+translator = Translator()
+
+example = "Bom dia pessoal, é um prazer estar aqui com vocês"
+example_translated = translator.translate(example, "en")
+print(example_translated)
