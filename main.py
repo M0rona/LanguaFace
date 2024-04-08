50c322c87c11a3572dcfbabd76f0fc90dfa26612
M0rona <gabrielmorona0229@gmail.com>
2024-04-08 14:23:46 -0300

    Primeiro protótipo de tela traduzindo para inglês

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-# UI
-#import tkinter
+import customtkinter as tk
+from translate import traduzir
+
+janela = tk.CTk()
+
+janela.geometry("700x500")
+janela.resizable(False, False)
+janela.title("LanguaFace")
+
+title = tk.CTkLabel(janela, text="Traduza o texto que quiser")
+title.pack(padx=10, pady=10)
+
+texto = tk.CTkEntry(janela, placeholder_text="Informe a frase a ser traduzida", width=500)
+texto.pack(padx=10)
+
+labelIdioma = tk.CTkLabel(janela, text="Informe o idioma")
+labelIdioma.pack(padx=10, pady=10)
+
+combo = tk.CTkComboBox(janela, values=["Inglês", "Português", "Chines", "Espanhol", "Francês", "Russo"])
+combo.pack(padx=10)
+
+def clique():
+    traducao = traduzir(texto)
+    print(traducao)
+
+
+botao = tk.CTkButton(janela, text="Traduzir", command=clique)
+botao.pack(padx=10, pady=30)
+
+janela.mainloop()
