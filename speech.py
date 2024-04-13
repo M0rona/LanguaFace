0736f57ee0a62043f1c166caa488fd581daad32d
Guichard <jeancharles.g.guichard@gmail.com>
2024-04-13 14:23:39 -0300

    speech test

--- speech.py
+++ speech.py
@@ -1,23 +1,27 @@
 import speech_recognition
 import pyttsx3
 
-recognizer = speech_recognition.Recognizer()
+recognizer= speech_recognition.Recognizer()
+
+languages= {
+    'portuguese': 'pt-BR',
+    'english': 'en-US',
+    'spanish': 'es-ES',
+    'franch': 'fr-FR',
+}
 
 while True:
-    
     try:
-        
+        print('Listening...')
         with speech_recognition.Microphone() as mic:
-            
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-            
-            text = recognizer.recognize_google_cloud(audio)
-            text = text.lower()
-            
-            print(f"Recognized {text}")
+            audio= recognizer.listen(mic)
 
-    except speech_recognition.UnknownValueError:
-        
-        recognizer = speech_recognition.Recognizer()
-        continue
+            text= recognizer.recognize_google(audio, language= languages['franch'])
+            text= text.lower()
+
+            print(f'You said: {text}')
+    
+    except speech_recognition.UnknownValueError():
+        recognizer= speech_recognition.Recognizer()
+        continue  
